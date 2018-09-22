3cf8cf487406c789ff3cc157440323f132b6a078
Volodymyr Myshko <volodymyrm@wix.com>
2018-09-22 23:47:27 +0300

    port set to 3000

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getOrdersFromServer, getOrdersFromCache } = require("./orders");
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
 const app = express();
 
 console.log("[server] init...");
