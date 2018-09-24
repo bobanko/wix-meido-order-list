6f95e96911fd634c4d67f7892ec7ef0ec6c08edb
Volodymyr Myshko <volodymyrm@wix.com>
2018-09-24 15:47:52 +0300

    da-users added to config for future

--- config.js
+++ config.js
@@ -1,4 +1,21 @@
 module.exports = {
-  serverUrl: 'https://analytics.getmeido.com/media/json?hash=',
-  defaultHash: 'wjdj1412sdasf', //4 floor
-}
+  serverUrl: "https://analytics.getmeido.com/media/json?hash=",
+  defaultHash: "wjdj1412sdasf", //4 floor
+  daUsers: [
+    "Mykyta Shulipa",
+    "Alina Kononenko",
+    "Vitalii Dvoretskyi",
+    "Andrii Umanskyi",
+    "Volodymyr Kaliuzhnyi",
+    "Kristina Lyba",
+    //"Volodymyr Myshko",
+    "Andrii Motsyk",
+    "Kostiantyn Ovsepian",
+    "Oleksandr Danylenko",
+    "Natalia Gagarina",
+    "Liudmyla Manziuk",
+    "Anton Berezhnyi",
+    "Mykhailo Khraban",
+    "Viktor Paladiichuk"
+  ]
+};
