4c3af29fba581c8b1a9719337e02e73f67188ea8
Volodymyr Myshko <volodymyrm@wix.com>
2018-09-25 00:44:04 +0300

    reducer usage

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,68 +1,57 @@
 import * as React from "react";
 import * as moment from "moment";
+import { connect } from "react-redux";
+
 import { Worker } from "./types";
 import { WorkerComponent } from "./worker";
-
-const { version } = require("../../package.json");
+import Updater from "./updater";
+import Progress from "./progress";
 
 import "./app.scss";
 
-type State = {
+type Props = {
   workers: Array<Worker>;
   isLoading: boolean;
   error?: Error;
+  updateWorkers: (string) => any;
 };
 
-export class App extends React.Component<any, State> {
-  state: State = {
-    isLoading: false,
-    workers: [],
-    error: null
-  };
-
+export class App extends React.Component<Props> {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
-    this.getOrders(location.search);
-  }
-
-  getOrders = params => {
-    this.setState({ isLoading: true });
-
-    fetch(`/orders/${params}`)
-      .then(data => data.json())
-      .then(workers => this.setState({ workers }))
-      .catch(error => this.setState({ error }))
-      .then(() => this.setState({ isLoading: false }));
-  };
-
   render() {
-    const { workers, isLoading, error } = this.state;
+    const { workers, isLoading, error } = this.props;
 
     const currentDate = moment().format("dddd, MMMM Do, YYYY");
-    const lastUpdateTime = moment().format(`ddd, MMM-D'YY HH:mm:ss`);
 
     return (
       <div>
+        <Progress isLoading={isLoading} />
+        <Updater />
+
         <h1>Orders for {currentDate}</h1>
 
         <div className="workers">
-          {error && <div>{error.message}</div>}
-          {isLoading && <div className="loader">Loading...</div>}
           {workers.map((worker, index) => (
             <WorkerComponent key={index} worker={worker} />
           ))}
           {!isLoading && workers.length === 0 && <h2>No orders</h2>}
         </div>
-
-        <div className="app-info">
-          <div>last update: {lastUpdateTime} </div>
-          <div>status: offline</div>
-          <div>ver. {version}</div>
-        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { workers, isLoading, error } = state;
+
+  return {
+    workers,
+    isLoading,
+    error
+  };
+};
+
+export default connect(mapStateToProps)(App);
