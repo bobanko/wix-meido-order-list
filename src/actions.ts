98e2d4ce63574707a58de1bd800de0067315a1d3
Volodymyr Myshko <volodymyrm@wix.com>
2018-09-25 01:58:13 +0300

    client refresh page when server version is newer

--- src/actions.ts
+++ src/actions.ts
@@ -3,6 +3,8 @@ export const RECEIVE_ORDERS = "RECEIVE_ORDERS";
 export const RECEIVE_ERROR = "RECEIVE_ERROR";
 export const UPDATE_APP = "UPDATE_APP";
 
+export const { version: clientVersion } = require("../package.json");
+
 export const requestOrders = params => ({
   type: REQUEST_ORDERS,
   params
@@ -20,16 +22,26 @@ export const receiveError = error => ({
   receivedAt: Date.now()
 });
 
-export const updateApp = () => ({
-  type: UPDATE_APP
-});
+// export const updateApp = () => ({
+//   type: UPDATE_APP
+// });
 
 // THUNKS ? where to move it?
 export const fetchWorkers = params => dispatch => {
   dispatch(requestOrders(params));
 
   return fetch(`/orders/${params}`) //todo: move fetch to api?
-    .then(data => data.json())
+    .then(response => {
+      const serverVersion = response.headers.get("version");
+
+      if (serverVersion !== clientVersion) {
+        //UPDATE APP
+        window.location.reload();
+      }
+
+      return response;
+    })
+    .then(response => response.json())
     .then(
       workers => dispatch(receiveOrders(workers)),
       error => dispatch(receiveError(error))
