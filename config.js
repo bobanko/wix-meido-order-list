0463e18c50163a5aac52a852828291e1d91f3c99
bobanko <mishko.vladimir@gmail.com>
2019-07-22 13:45:29 +0300

    config: all floors listed

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-const floors = new Map([[4, "wjdj1412sdasf"], [6, "12evsadba2f"]]);
+const floors = new Map([
+  [4, "wjdj1412sdasf"],
+  [5, "wjdj1410sdadd"],
+  [6, "12evsadba2f"],
+  [8, "bas45143568e"] //da
+]);
 
 module.exports = {
   serverUrl: "https://analytics.getmeido.com/media/json?hash=",
